3dccb0e39c6826a3101880a83de036fbb2f13624
Alexey <alexey_penkov@list.ru>
2022-05-13 18:02:53 +0800

    start

---
+++ EducationMap/EducationMap/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EducationMap
+//
+//  Created by Алексей Пеньков on 13.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
